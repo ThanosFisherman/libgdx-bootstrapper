8c4297bda35fa1250272483597ccccbedcdc3a49
Thanos Psaridis <psaridis@gmail.com>
2023-11-28 15:32:39 +0200

    cleanups

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     //id("com.android.library") version Versions.Plugins.agpVersion apply false
 //    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
 //    id("org.jetbrains.kotlin.jvm") version "1.9.21" apply false
-    base
     id("game.dependencies")
 }
 
@@ -16,13 +15,4 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.1.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
     }
-}
-
-allprojects {
-    group = "io.github.thanosfisherman.game"
-    version = "1.0-SNAPSHOT"
-}
-
-tasks.getByName<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
